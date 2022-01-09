2d8c77a2aeba58eb4f4c5916a26d97c81d18681c
swillrodt <sumnerwillrodt@gmail.com>
2022-01-09 13:25:52 -0800

    setup contact section

--- src/App.js
+++ src/App.js
@@ -1,15 +1,33 @@
 import React, { useState } from 'react';
 import Header from './assets/components/Header/Header';
 import About from './assets/components/Nav/About';
+import ContactForm from './assets/components/Nav/Contact';
 
 function App() {
   //const [currentPage, handlePageChange] = useState("Homepage");
+  const [navTitles] = useState(["About", "Portfolio", "Resume", "Contact"]);
+
+  const [currentPage, setCurrentPage] = useState(navTitles[0]);
+
+  const [contactSelected, setContactSelected] = useState(false);
 
   return (
     <div>
-      <Header></Header>
+      <Header
+        navTitles={navTitles}
+        setCurrentPage={setCurrentPage}
+        currentPage={currentPage}
+        contactSelected={contactSelected}
+        setContactSelected={setContactSelected}
+      ></Header>
       <main>
-        <About></About>
+        {!contactSelected ? (
+          <About></About>
+          // <Portfolio></Portfolio>
+          // <Resume></Resume>
+        ) : (
+          <ContactForm></ContactForm>
+        )}
       </main>
     </div>
   );
--- src/assets/components/Header/Header.js
+++ src/assets/components/Header/Header.js
@@ -1,9 +1,21 @@
-import React from "react";
-import logo from '../../../SW-Logo.svg';
-
+import React, { useEffect } from "react";
+import logo from "../../../SW-Logo.svg";
+import { capitalizeFirstLetter } from "../../../utils/helpers";
 
 function Header(props) {
-  const navTitles =  ["About", "Portfolio", "Resume", "Contact"];
+  // const navTitles =  ["About", "Portfolio", "Resume", "Contact"];
+  const {
+    navTitles = [],
+    setCurrentPage,
+    contactSelected,
+    currentPage,
+    setContactSelected,
+  } = props;
+
+  useEffect(() => {
+    document.title = capitalizeFirstLetter(currentPage.name);
+  }, [currentPage]);
+
   return (
     <header className="flex-row px-2">
       <div id="logo-area">  
@@ -12,11 +24,19 @@ function Header(props) {
       </div>
       <nav id="nav-bar">
         <ul id="nav-list" className="flex-row">
-          {navTitles.map(navTitle => {
+          <li className="mx-2">
+            <a data-testid="about" href="#about" onClick={() => setContactSelected(false)}>
+              About me
+            </a>
+          </li>
+          <li className={`mx-2 ${contactSelected && 'navActive'}`}>
+            <span onClick={() => setContactSelected(true)}>Contact</span>
+          </li>
+          {/* {navTitles.map(navTitle => {
             <li className="nav-item" key={ navTitle }>
               <a href={"#" + navTitle.toLowerCase()} onClick={() => props.handlePageChange(navTitle)} className={ props.currentPage === navTitle ? "navTitle active" : "nav-link"}>{ navTitle }</a>
             </li>
-          })}
+          })} */}
         </ul>
 
       </nav>
