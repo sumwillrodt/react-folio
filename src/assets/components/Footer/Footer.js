21019c7286c2f34f14203ea2c475ade16abebbbd
swillrodt <sumnerwillrodt@gmail.com>
2022-01-20 09:47:52 -0800

    style footer

--- src/assets/components/Footer/Footer.js
+++ src/assets/components/Footer/Footer.js
@@ -6,23 +6,26 @@ import InstagramIcon from '@mui/icons-material/Instagram';
 
 function Footer() {
   return (
-    <div id="social-container" className="">
-      <IconButton
-        sx={{ color: 'rgba(255, 255, 255, 0.5)' }}
-        aria-label={`GitHub`}
-        href={`https://github.com/sumwillrodt`}><GitHubIcon />
-      </IconButton>
-      <IconButton
-        sx={{ color: 'rgba(255, 255, 255, 0.5)' }}
-        aria-label={`LinkedIn`}
-        href={`https://www.linkedin.com/in/sumnerwillrodt/`}><LinkedInIcon />
-      </IconButton>
-      <IconButton
-        sx={{ color: 'rgba(255, 255, 255, 0.5)' }}
-        aria-label={`Instagram`}
-        href={`https://www.instagram.com/sumwillrodt/`}><InstagramIcon />
-      </IconButton> 
-    </div>
+    <footer>
+      <h4>Find me on</h4>
+      <div>
+        <IconButton
+          sx={{ color: 'rgba(34, 34, 34)' }}
+          aria-label={`GitHub`}
+          href={`https://github.com/sumwillrodt`}><GitHubIcon />
+        </IconButton>
+        <IconButton
+          sx={{ color: 'rgba(34, 34, 34)' }}
+          aria-label={`LinkedIn`}
+          href={`https://www.linkedin.com/in/sumnerwillrodt/`}><LinkedInIcon />
+        </IconButton>
+        <IconButton
+          sx={{ color: 'rgba(34, 34, 34)' }}
+          aria-label={`Instagram`}
+          href={`https://www.instagram.com/sumwillrodt/`}><InstagramIcon />
+        </IconButton> 
+      </div>
+    </footer>
   )
 }
 
